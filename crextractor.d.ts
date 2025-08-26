03c74c53a3330d6ca83ea948379c49fc6f031712
Vitaly Gashkov <vitalygashkov@vk.com>
2025-08-26 12:22:10 +0500

    fix: improve types

--- crextractor.d.ts
+++ crextractor.d.ts
@@ -1,10 +1,24 @@
-export function extractSecrets(): Promise<{
+export type CrunchyrollAppCredentials = {
   // Crunchyroll app ID
   id: string;
   // Crunchyroll app secret
   secret: string;
   // Base64 encoded `id:secret` string
   encoded: string;
-  // HTTP header with Basic Authorization to access Crunchyroll mobile APIs
+  // Ready HTTP header in the format `Basic <encoded>`, can be used to access some Crunchyroll APIs
   authorization: string;
-}>;
+};
+
+/**
+ * Extract credentials from the Crunchyroll Android APK using jadx.
+ */
+export function extract(options?: {
+  target?: 'mobile' | 'tv';
+  output?: string;
+  cleanup?: boolean;
+}): Promise<CrunchyrollAppCredentials>;
+
+/**
+ * Fetch ready credentials from the GitHub repository.
+ */
+export function pull(options?: { target: 'mobile' | 'tv' }): Promise<CrunchyrollAppCredentials>;
